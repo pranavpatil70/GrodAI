145d591d64e2bc5bfe8ff49865cd4cb9493d2496
Pranav Patil <pvp3400@gmail.com>
2024-07-12 23:29:18 +0530

    comments added

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#This project is the property of Pranav Patil
+#YouTube channel: @thecuriouspranav
 # app.py
 from flask import Flask, render_template, request, redirect, url_for, session, send_file
 import boto3
